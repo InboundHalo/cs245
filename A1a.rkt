9d64a1e6e9ef75ff9227e7df862aec2f8fe5713c
InboundHalo <kieranbeals@gmail.com>
2025-09-09 13:50:19 -0400

    Fixed median function will now get the median instead of the average

--- A1a.rkt
+++ A1a.rkt
@@ -1,4 +1,15 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname A1a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(define (median x y z) (/(+ x y z)3))
+(define (median x y z) (cond
+  [(is_median x y z) y]
+  [(is_median y x z) x]
+  [(is_median y z x) z]))
+
+;; checks if the number in the middle is the median
+(define (is_median x y z)
+  (or
+   (and (< y z) (< x y))
+   (and (> y z) (> x y))
+  )
+)
