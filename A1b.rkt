68a55d99dd08babfd644106175bf7b60d2d543e2
InboundHalo <kieranbeals@gmail.com>
2025-09-10 12:01:06 -0400

    Fixed division by zero error

--- A1b.rkt
+++ A1b.rkt
@@ -3,7 +3,13 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname A1b) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; six numbers x1, y1, x2, y2,x3, y3, where (x1, y1), (x2, y2) and (x3, y3) are distinct points on the Cartesian plane
 ;; will return true if all points fall in the same line
-(define (collinear x1 y1 x2 y2 x3 y3) (= (slope x1 y1 x2 y2) (slope x2 y2 x3 y3)))
+(define (collinear x1 y1 x2 y2 x3 y3) (cond
+                                        ;; Check for / by 0
+                                        [(and (= (- y2 y1) 0) (= (- y3 y1) 0)) true]
+                                        [(= (- y2 y1) 0) false]
+                                        [(= (- y3 y1) 0) false] 
+                                        [else (= (slope x1 y1 x2 y2) (slope x2 y2 x3 y3))]))
 
 ;; returns the slope of a line
-(define (slope x1 y1 x2 y2) (/(- x2 x1) (- y2 y1)))t2
+(define (slope x1 y1 x2 y2) (/(- x2 x1) (- y2 y1)))
+
