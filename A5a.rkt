f144b776d2efabd8565d764063ce501fce8ef212
InboundHalo <kieranbeals@gmail.com>
2025-10-02 15:19:53 -0400

    Update A5a.rkt
    Passed all tests

--- A5a.rkt
+++ A5a.rkt
@@ -1,14 +1,23 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname A5a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname A5a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(check-expect (take (list 1 2 3 4 5 6 7) 0) empty)
 (check-expect (take (list 1 2 3 4 5 6 7) 3) (list 1 2 3))
 (check-expect (take (list 1 5 2 4 5 6 7) 3) (list 1 5 2))
+(check-expect (take (list 1 2 3) 4) (list 1 2 3))
 ;; take: (listof Any) Nat -> (listof Any)
-(define (take lst n) (if (= n 0) empty
-                         (cons (first lst) (take (rest lst) (sub1 n)))))
+(define (take lst n) (cond
+                       [(<= n 0) empty]
+                       [(empty? lst) empty]
+                       [else (cons (first lst) (take (rest lst) (sub1 n)))]
+                       ))
 
-(check-expect (drop (list 1 2 3 4 5 6 7) 1) (list 1 2 3 4 5 6))
-(check-expect (drop (list 1 2 3 4 5 6 7) 3) (list 1 2 3 4))
-(check-expect (drop (list 1 5 2 4 5 6 7) 3) (list 1 5 2 4))
+(check-expect (drop (list 1 2 3 4 5 6 7) 0) (list 1 2 3 4 5 6 7))
+(check-expect (drop (list 1 2 3 4 5 6 7) 1) (list 2 3 4 5 6 7))
+(check-expect (drop (list 1 2 3 4 5 6 7) 3) (list 4 5 6 7))
+(check-expect (drop (list 1 5 2 4 5 6 7) 3) (list 4 5 6 7))
+(check-expect (drop (list 1 2 3) 4) empty)
 ;; drop (listof Any) Nat -> (listof Any)
-(define (drop lst n) (take lst (- (length lst) n)))
+(define (drop lst n) (cond [(= n 0) lst]
+                           [(empty? lst) empty]
+                           [else (drop (rest lst) (sub1 n))]))
